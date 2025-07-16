b6edd96ce9da9c3dcdab338ce5bf971db938febc
hhayan <chcod35@gmail.com>
2025-07-16 18:20:02 +0900

    python dat3

--- SQL/project/2-1.sql
+++ SQL/project/2-1.sql
@@ -1,6 +1,9 @@
--- 직원별 담당 고객 수 집계
+-- 직원별 /  담당 고객 / 수 집계
 -- 각 직원(employee_id, first_name, last_name)이 담당하는 고객 수를 집계하세요.
 -- 고객이 한 명도 없는 직원도 모두 포함하고, 고객 수 내림차순으로 정렬하세요.
+
+c.담당직원 = e.e_id
+e.담당고객 = c.c_id
 
 SELECT
 	COUNT(DISTINCT c.customer_id) AS 고객수,
@@ -15,4 +18,10 @@ ORDER BY 고객수 DESC;
 SELECT * FROM employees;
 SELECT * FROM customers;
 
--- join 컬럼? 조인 기준?
+-- 조인 기준: N 대 N일 때,
+--Group by 여러 개일 때:  first_name과 last_name을 추가했기 때문에, 
+같은 employee_id 내에서 first_name과 last_name의 조합별로 세분화된 그룹이 형성됩니다.
+-- ON절의 역할:
+ON 컬럼명이 다르더라도, 두 열의 데이터 타입이 호환 가능하면(예: 둘 다 정수형) 가능
+데이터 타입이 호환되고(예: 둘 다 INT), 
+SQL은 열 이름이 아니라 값의 일치 여부를 기준으로 JOIN을 수행합니다
